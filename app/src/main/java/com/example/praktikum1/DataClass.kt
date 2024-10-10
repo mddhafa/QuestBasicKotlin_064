7f2188e603fbfd919447e80fd884cc11fbbb6c47
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-10-10 21:36:54 +0700

    Membuat fungsi utama untuk mendeklarasikan fungsi dan menambahkan variable data lalu mencetak variabel tersebut

--- app/src/main/java/com/example/praktikum1/DataClass.kt
+++ app/src/main/java/com/example/praktikum1/DataClass.kt
@@ -9,3 +9,11 @@ data class DataClass(
     val id : Int,
     val email: String
 )
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    // fungsi equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) //false

1cd1cb279e694ea0a2636dc67f6626fa96b17094
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-10-10 21:46:55 +0700

    Membuat fungsi contoh list

--- app/src/main/java/com/example/praktikum1/Collections.kt
+++ app/src/main/java/com/example/praktikum1/Collections.kt
@@ -34,6 +34,12 @@ fun ContohList(){
     val shapeLocked: List<String> = shape
     println(shapeLocked)
 }
+// set adalah kumpulan data yang memiliki posisi atau urutan dan hanya menyimpan data unik(tidak ada duplikasi)
+// set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// set read only menggunakan setOf
+// set mutable menggunakan mutableSetOf
+
 
 fun main(){
     ContohList()

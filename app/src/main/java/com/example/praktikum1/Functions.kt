eb06e7daf52270140a970d2d9c99144a24fc6cb8
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-10-11 20:49:46 +0700

    Membuat fungsi withReturn

--- app/src/main/java/com/example/praktikum1/Functions.kt
+++ app/src/main/java/com/example/praktikum1/Functions.kt
@@ -1,6 +1,6 @@
 package com.example.praktikum1
 
-fun withoutparameter(){
+fun withoutParameter(){
     println("== withoutParameter ==")
     println("Hello, World!")
 }
@@ -26,3 +26,8 @@ fun withDefaultParameter(name: String = "Indra", age: Int) {
     println("== withDefaultParameter ==")
     println("Hello, $name! You are $age years old.")
 }
+
+fun withReturn(panjang : Int, lebar : Int) {
+    return println(panjang * lebar)
+}
+

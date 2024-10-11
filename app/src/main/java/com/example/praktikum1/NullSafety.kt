65ec64dd7fb432d3695de0baf56e0869002ae7e5
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-10-11 17:54:45 +0700

    Membuat fungsi nullsafety untuk menajalankan null

---
+++ app/src/main/java/com/example/praktikum1/NullSafety.kt
@@ -0,0 +1,28 @@
+package com.example.praktikum1
+
+fun nullSafety(){
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler eror
+    // neverNull = null
+
+    // nullable has nullable String type
+    var nullable: String? = "you can keep a null here"
+    // Thiss is ok
+    nullable = null
+
+
+    // Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    }else{
+        println("inferredNonNull is not null")
+    }
+
+    // Safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+}
+
+
